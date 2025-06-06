63c2b5a7eba81cd33c167a849e414421ab0d4640
Henrique Furtado <161591169+henrique-furtado47@users.noreply.github.com>
2025-06-06 08:34:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,25 +3,35 @@ let lista = document.querySelector("#lista");
 
 botao.addEventListener("click", function (evt) {
   evt.preventDefault();
-  console.log("Foi");
   let campo = document.querySelector("input");
   let item = document.createElement("li");
+
+  item.innerHTML = `
+    <input type='checkbox' class='checkbox'>
+    <span class='texto'>${campo.value}</span>
+    <span class='remover'>Delete</span>
+    <span class='editar'>Edit</span>
+  `;
+
   lista.appendChild(item);
-  item.innerHTML =
-    "<span class='texto'>" +
-    campo.value +
-    "</span> <span class='remover'>Delete</span> <span class='editar'>Edit</span>";
   campo.value = "";
 });
 
 lista.addEventListener("click", function (evt) {
-  evt.preventDefault();
   let elemento = evt.target;
-  console.log(elemento.className);
-  if (elemento.className == "remover") {
+
+  if (elemento.className === "remover") {
     elemento.parentElement.remove();
-  } else if (elemento.className == "editar") {
-    elemento.parentElement.querySelector(".texto").contentEditable = true;
-    elemento.parentElement.querySelector(".texto").focus();
+  } else if (elemento.className === "editar") {
+    const texto = elemento.parentElement.querySelector(".texto");
+    texto.contentEditable = true;
+    texto.focus();
+  } else if (elemento.className === "checkbox") {
+    const texto = elemento.parentElement.querySelector(".texto");
+    if (elemento.checked) {
+      texto.classList.add("riscado");
+    } else {
+      texto.classList.remove("riscado");
+    }
   }
 });
